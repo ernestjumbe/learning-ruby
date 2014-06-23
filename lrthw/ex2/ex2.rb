e941510f5ea624b3564e1116475e2941714e68a3
ernestjumbe <e2jeyy@gmail.com>
2014-06-23 05:34:03 +0200

    lrthw up to ex5

---
+++ lrthw/ex2/ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can reaf you program Later.
+# Wnything after the # is ignored by Ruby.
+
+puts "I coud have cosr like this." # and the comment afte is ignored
+
+# You can alos use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
