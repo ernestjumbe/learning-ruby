384ed76ffdd787493184439aea640d5068bc177d
ernestjumbe <e2jeyy@gmail.com>
2016-09-15 23:32:26 +0200

    Up to ex8

--- lrthw/ex5/ex5.rb
+++ lrthw/ex5/ex5.rb
@@ -6,14 +6,13 @@ my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
-puts "Let's talk about %s." % my_name
-puts "He's %d inches tall." % my_height
-puts "He's %d pounds heavy." % my_weight
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
-puts "His teeth are usually %s depending on the coffee." % my_teeth
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee." 
 
 # this line is tricky, try to get it exactly right
-puts "If I add %d, %d and %d I get %d." % [
-    my_age, my_height, my_weight, my_age + my_height + my_weight]
+puts "If I add #{my_age}, #{my_height} and #{my_weight} I get #{my_age + my_height + my_weight}."
     
